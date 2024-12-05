27f252df2204d633a69da5f40fc0ac8c5cc35f71
Ajay Tatachar <ajay_m_tatachar@apple.com>
2024-12-05 15:44:45 -0500

    doomgeneric: clang-format and request window size

--- doomgeneric/doomgeneric/doomgeneric_mako.c
+++ doomgeneric/doomgeneric/doomgeneric_mako.c
@@ -2,69 +2,76 @@
 #include "doomgeneric.h"
 #include "doomkeys.h"
 #include "scancode.h"
-#include <ui.h>
-#include <stdio.h>
 #include <mako.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+#include <ui.h>
 
 static uint32_t start_time = 0;
-static uint8_t window_init = 0;
-static uint32_t window_w = 0;
-static uint32_t window_h = 0;
+static bool window_init = false;
 static uint32_t *ui_buf = 0;
 
 #define KEYQUEUE_SIZE 16
 
-struct keyqueue_entry {
+struct keyqueue_entry
+{
   uint8_t key;
-  uint8_t pressed;
+  bool pressed;
 };
 
 static unsigned keyqueue_read_idx = 0;
 static unsigned keyqueue_write_idx = 0;
 static struct keyqueue_entry keyqueue[KEYQUEUE_SIZE];
 
-static uint8_t convert_to_doom_key(uint8_t key, uint8_t *out) {
+static uint8_t convert_to_doom_key(uint8_t key, uint8_t *out)
+{
   switch (key) {
-  case KB_SC_ESC:
-    *out = KEY_ESCAPE;
-    return 1;
-  case KB_SC_ENTER:
-    *out = KEY_ENTER;
-    return 1;
-  case KB_SC_A:
-    *out = KEY_FIRE;
-    return 1;
-  case KB_SC_SPACE:
-    *out = KEY_USE;
-    return 1;
-  case KB_SC_RSHIFT:
-    *out = KEY_RSHIFT;
-    return 1;
-  case KB_SC_UP:
-    *out = KEY_UPARROW;
-    return 1;
-  case KB_SC_DOWN:
-    *out = KEY_DOWNARROW;
-    return 1;
-  case KB_SC_LEFT:
-    *out = KEY_LEFTARROW;
-    return 1;
-  case KB_SC_RIGHT:
-    *out = KEY_RIGHTARROW;
-    return 1;
-  case KB_SC_Y:
-    *out = 'y';
-    return 1;
-  default:
-    return 0;
+    case KB_SC_ESC:
+      *out = KEY_ESCAPE;
+      return 1;
+    case KB_SC_ENTER:
+      *out = KEY_ENTER;
+      return 1;
+    case KB_SC_QUOTE:
+      *out = KEY_FIRE;
+      return 1;
+    case KB_SC_SPACE:
+      *out = KEY_USE;
+      return 1;
+    case KB_SC_RSHIFT:
+      *out = KEY_RSHIFT;
+      return 1;
+    case KB_SC_W:
+    case KB_SC_UP:
+      *out = KEY_UPARROW;
+      return 1;
+    case KB_SC_S:
+    case KB_SC_DOWN:
+      *out = KEY_DOWNARROW;
+      return 1;
+    case KB_SC_A:
+    case KB_SC_LEFT:
+      *out = KEY_LEFTARROW;
+      return 1;
+    case KB_SC_D:
+    case KB_SC_RIGHT:
+      *out = KEY_RIGHTARROW;
+      return 1;
+    case KB_SC_Y:
+      *out = 'y';
+      return 1;
+    default:
+      return 0;
   }
 }
 
-static void buffer_key(uint8_t pressed, uint8_t code) {
+static void buffer_key(bool pressed, uint8_t code)
+{
   uint8_t doom_key = 0;
-  if (!convert_to_doom_key(code, &doom_key)) return;
+  if (!convert_to_doom_key(code, &doom_key))
+    return;
   keyqueue[keyqueue_write_idx].key = doom_key;
   keyqueue[keyqueue_write_idx].pressed = pressed;
   keyqueue_write_idx = (keyqueue_write_idx + 1) % KEYQUEUE_SIZE;
@@ -74,16 +81,18 @@ static void keyboard_handler(uint8_t code)
 {
   if (code & 0x80) {
     code &= 0x7F;
-    buffer_key(0, code);
-  } else buffer_key(1, code);
+    buffer_key(false, code);
+  } else
+    buffer_key(true, code);
 }
 
 void DG_Init()
 {
   priority(2);
 
-  int32_t res = ui_acquire_window("DOOM");
-  if (res < 0) return;
+  int32_t res = ui_acquire_window("DOOM", DOOMGENERIC_RESX, DOOMGENERIC_RESY);
+  if (res < 0)
+    return;
   ui_buf = (uint32_t *)res;
 
   // wait until window creation event
@@ -93,44 +102,39 @@ void DG_Init()
     return;
 
   start_time = systime();
-  window_init = 1;
-  window_w = ev.width; window_h = ev.height;
+  window_init = true;
   uint32_t size = ev.width * ev.height * 4;
   memset(ui_buf, 0, size);
 }
 
 void DG_DrawFrame()
 {
-  if (!window_init) return;
+  if (!window_init)
+    return;
 
   while (ui_poll_events()) {
-    ui_event_t ev; ui_next_event(&ev);
+    ui_event_t ev;
+    ui_next_event(&ev);
     if (ev.type == UI_EVENT_WAKE)
       priority(2);
     if (ev.type == UI_EVENT_SLEEP)
       priority(1);
-    if (ev.type == UI_EVENT_KEYBOARD) keyboard_handler(ev.code);
+    if (ev.type == UI_EVENT_KEYBOARD)
+      keyboard_handler(ev.code);
   }
 
-  double min_ratio = (double)window_h / (double)DOOMGENERIC_RESY;
-  if ((double)window_w / (double)DOOMGENERIC_RESX < min_ratio)
-    min_ratio = (double)window_w / (double)DOOMGENERIC_RESX;
-  if (min_ratio > 1) min_ratio = 1; // don't upscale for now -- maybe change later?
-
-  for (int i = 0; i < DOOMGENERIC_RESY; ++i) {
-    for (int j = 0; j < DOOMGENERIC_RESX; ++j) {
-      uint32_t x = j * min_ratio;
-      uint32_t y = i * min_ratio;
-      ui_buf[y * window_w + x] = DG_ScreenBuffer[i * DOOMGENERIC_RESX + j];
-    }
+  for (unsigned y = 0; y < DOOMGENERIC_RESY; ++y) {
+    memcpy32(
+      ui_buf + y * DOOMGENERIC_RESX, DG_ScreenBuffer + y * DOOMGENERIC_RESX, DOOMGENERIC_RESX);
   }
 
-  ui_redraw_rect(0, 0, window_w, window_h);
+  ui_redraw_rect(0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY);
 }
 
 int DG_GetKey(int *pressed, unsigned char *key)
 {
-  if (keyqueue_read_idx == keyqueue_write_idx) return 0;
+  if (keyqueue_read_idx == keyqueue_write_idx)
+    return 0;
 
   *pressed = keyqueue[keyqueue_read_idx].pressed;
   *key = keyqueue[keyqueue_read_idx].key;
@@ -139,10 +143,13 @@ int DG_GetKey(int *pressed, unsigned char *key)
 }
 
 void DG_SleepMs(uint32_t ms)
-{ msleep(ms); }
+{
+  msleep(ms);
+}
 
-void DG_SetWindowTitle(const char *title)
-{}
+void DG_SetWindowTitle(const char *title) {}
 
 uint32_t DG_GetTicksMs()
-{ return systime() - start_time; }
+{
+  return systime() - start_time;
+}
